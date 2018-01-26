3a7dbe6d2fd22f6ae448699291fad0151560339d
hatice.ozdemir <hatice.ozdemir@trendyol.com>
2018-01-26 15:49:38 +0300

    Encapsulation is Made State counters are added to the class

--- src/main/java/simulation/State.java
+++ src/main/java/simulation/State.java
@@ -6,19 +6,27 @@ import java.util.ArrayList;
  * Created by hatice.ozdemir on 27.12.2017.
  */
 public class State {
-    public Integer clock;
-    public ArrayList<Integer> totalCustomerIdList;
+    private Integer clock;
+    private ArrayList<Integer> totalCustomerIdList;
+    private Integer numberOfFails;
+    private Integer numberOfSuccess;
 
     public State(){
+        numberOfFails = 0;
+        numberOfSuccess = 0;
         clock = 0;
         totalCustomerIdList = new ArrayList<Integer>();
     }
 
     public Integer getClock(){return clock;}
-
     public void setClock(Integer clock){this.clock = clock;}
 
     public ArrayList<Integer> getTotalCustomerIdList(){return totalCustomerIdList;}
-
     public void addCustomer(Integer customerId){totalCustomerIdList.add(customerId);}
+
+    public Integer getNumberOfFails () {return this.numberOfFails;}
+    public void setNumberOfFails(Integer numberOfFails){this.numberOfFails = numberOfFails;}
+
+    public Integer getNumberOfSuccess() {return this.numberOfSuccess;}
+    public void setNumberOfSuccess(Integer numberOfSuccess) {this.numberOfSuccess = numberOfSuccess;}
 }
